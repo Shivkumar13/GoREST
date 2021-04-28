b173b47cc150929c5646be279acf203d65c410c3
Shivkumar13 <shivkumar.ople97@gmail.com>
2021-04-28 23:45:06 +0530

    Day 2: Added a 404 page And implemented a httprouter

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -10,14 +12,22 @@ func main() {
 	// The "HandleFunc" method accepts a path and a function as arguments
 	// Yes we can pass functions as arguments, and even treat them like variables in Go
 	// However, the handler function has to have the appropriate signature (as desribed by the "handler" function below)
+	router := httprouter.New()
+	//router.Get("/", Index)
+	router.GET("/hello/:name", Hello)
 
-	http.HandleFunc("/", handler)
+	//	http.HandleFunc("/", handler)
 
 	// After defining our server, we finally "listen and serve" on port 8080
 	// The Second argument is the handler, which we will comet to later on, but now it is left as nil
 	// and the handler defined above (in "HandleeFunc") is used
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", router)
 
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
 // "handler" is our handler function. It has to follow the function signature of a ResponseWriter and Request type
